86049f65c2e8115640adb12ad5c7ee20e4152a6e
fadhil1806 <fadhil8637@smk.belajar.id>
2024-08-30 21:11:32 +0700

    finally tetapi no 5 dan 6 masih belum

--- design/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.getElementById('menuToggle');
-    const navLinks = document.getElementById('navLinks');
-
-    menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        navLinks.classList.toggle('active');
-    });
-});
---
+++ hosting.md
@@ -0,0 +1,8 @@
+# Desain
+https://fadhil1806.github.io/selekda-fadhil/design/index.html
+
+# Game
+https://fadhil1806.github.io/selekda-fadhil/game/index.html
+
+# Apps: 
+https://fadhil1806.github.io/selekda-fadhil/apps/index.html
